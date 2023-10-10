3345268f6147749e58245356efb86186643b43f6
wooki <wooki@datacrush.ai>
2023-10-10 23:08:36 +0900

    [feat] 20231010 포스트수정

--- public/posts/blog.js
+++ public/posts/blog.js
@@ -4,7 +4,7 @@ export const blogs = [
     slug: "0",
     title: `Next.js 13으로 불편하게 Github Blog 만들기
     `,
-    content: `/posts/2023/10/05.html`,
+    content: `/posts/2023/10/10.html`,
     description: `이번 포스트에서는 Next.js 13으로 Github Blog를 만들도록 하겠습니다.`,
     summary: `Next.js 13으로 Github Blog를 만드는 방법입니다.
     이 방법은 원하는 커스터마이징이 가능한 대신 불편함을 감수해야 합니다.
@@ -25,6 +25,6 @@ export const blogs = [
       "/posts/2023/10/week_1/blog.png",
     ],
     thumbnail: "/image/2023/10/06/Nextjs-logo.png",
-    create: "2023년 10월 05일",
+    create: "2023년 10월 10일",
   },
 ];
